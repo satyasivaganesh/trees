32a01728f41c4917e3b8a7f6ae32c7d6c635d6ef
satyasivaganesh <84192998+satyasivaganesh@users.noreply.github.com>
2021-12-07 14:17:49 +0530

    Create post order.cpp

---
+++ post order.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void postorder(TreeNode* root,vector<int> &v)
+    {
+        if(root)
+        {
+            postorder(root->left,v);
+            postorder(root->right,v);
+            v.push_back(root->val);
+        }
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>v;
+        postorder(root,v);
+        return v;
+        
+        
+        
+    }
+};
